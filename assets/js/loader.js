56fb0458c7a2e56f05e4fc3782df34d1f8dfa73a
jairo51067 <jairocardenas05@gmail.com>
2026-04-13 12:50:22 -0400

    Insertar documentacion

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,3 +1,7 @@
+/* This JavaScript code is adding an event listener to the `load` event of the `window` object. When
+the window has finished loading (i.e., when all resources have been loaded), the code inside the
+arrow function will be executed. */
+
 window.addEventListener("load", () => {
   const loader = document.getElementById("loader");
   loader.style.opacity = 0;
